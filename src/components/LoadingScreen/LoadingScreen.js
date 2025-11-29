9df64c27ab1182e07e51a60032bb8bd8eaea6ebb
jeeva <jeeva@kongu.ac.in>
2025-11-29 14:17:28 +0530

    vercel update-20

--- src/components/LoadingScreen/LoadingScreen.js
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -16,7 +16,7 @@ const LoadingScreen = () => {
         <div className="circle2"></div>
       </div>
       <div className="text">
-        <strong>Develop by AKSHAY</strong>Loading.....
+        <strong>Loading.....</strong>
       </div>
     </Box>
   );
